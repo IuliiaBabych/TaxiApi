805ab3a2d2d8ac9388ed7ccb3cc8e84dbb5b23f2
y.babych <y.babych@kdd-digital.de>
2023-11-07 10:54:04 +0200

    add empty projets for upcomming structure

---
+++ Controllers/HealthCheckController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaxiApi.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class HealthCheckController : ControllerBase
+	{
+
+		private readonly ILogger<HealthCheckController> _logger;
+
+		public HealthCheckController(ILogger<HealthCheckController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IActionResult Get() => Ok();
+	}
+}
